4e65009e70560d8c2be2e4c4aae32aec990f3b24
Leandro <le.s.kuranaga@hotmail.com>
2019-08-18 18:44:57 -0300

    SplashScreen funcionando nas duas plataformas mas precisa de correção

--- android/app/src/main/java/com/xrun/MainActivity.java
+++ android/app/src/main/java/com/xrun/MainActivity.java
@@ -3,10 +3,11 @@ package com.xrun;
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
+    //private static int SPLASH_TIME_OUT = 3000;
 
     /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
+     * Returns the name of the main component registered from JavaScript. This is
+     * used to schedule rendering of the component.
      */
     @Override
     protected String getMainComponentName() {
